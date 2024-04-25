af26b07124a16b989e0d503e60425bc0965b353d
hiletmis <hiletmis@gmail.com>
2024-04-25 13:26:42 +0300

    Delete unsused files

--- contracts/test/MockSelfMulticall.sol
+++
@@ -1,7 +0,0 @@
-// SPDX-License-Identifier: MIT
-pragma solidity ^0.8.4;
-
-import "../utils/SelfMulticall.sol";
-import "./MockMulticallTarget.sol";
-
-contract MockSelfMulticall is SelfMulticall, MockMulticallTarget {}
