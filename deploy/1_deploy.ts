9fede7bdee7ffefd4d13730cff1a7bbbdb4f1a50
hiletmis <hiletmis@gmail.com>
2024-04-25 15:42:33 +0300

    Update .gitignore and deploy scripts

--- deploy/1_deploy.ts
+++ deploy/1_deploy.ts
@@ -1,26 +1,21 @@
 import { deployments, ethers, network } from 'hardhat';
 
 import { chainsSupportedByDapis } from '../data/chain-support.json';
-import managerMultisigAddresses from '../data/manager-multisig.json';
 
 module.exports = async () => {
   const { deploy, log } = deployments;
   const [deployer] = await ethers.getSigners();
 
-  if (Object.keys(managerMultisigAddresses).includes(network.name)) {
-    const mockContract = 'Contract5';
-    if (chainsSupportedByDapis.includes(network.name)) {
-      await deployments.get(mockContract).catch(async () => {
-        log(`Deploying ${mockContract}...`);
-        return deploy(mockContract, {
-          from: deployer!.address,
-          log: true,
-          deterministicDeployment: process.env.DETERMINISTIC ? ethers.ZeroHash : '',
-        });
+  const mockContract = 'Contract5';
+  if (chainsSupportedByDapis.includes(network.name)) {
+    await deployments.get(mockContract).catch(async () => {
+      log(`Deploying ${mockContract}...`);
+      return deploy(mockContract, {
+        from: deployer!.address,
+        log: true,
+        deterministicDeployment: process.env.DETERMINISTIC ? ethers.ZeroHash : '',
       });
-    }
-  } else {
-    throw new Error(`${network.name} is not supported`);
+    });
   }
 };
 module.exports.tags = ['deploy'];
